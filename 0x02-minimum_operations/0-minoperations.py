39739338801868d1facbb6b6b2a3cad950118257
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-06 23:44:23 +0300

    final idea

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,22 +2,26 @@
 """Minimum Operations required to generate H n times
    Operations that can be performed include CopyAll and Paste
 """
+import math
+
+
 def minOperations(n):
-    """Returns an integer of the number of times we can copy and paste H
-    If n is impossible to achieve, return 0
+    """"Loop over all possible prime factors up to the square root
     """
-    if n <= 1:
+    if n < 1:
         return 0
-    # start with one
-    h_count = 1
-    times = 0
-    while n > 1:
-        if n % h_count == 0:
-            times += 2
-            h_count += 1
-            n = n - h_count
+    
+    operations = 0
+    i = 2
+    
+    while i <= math.sqrt(n):
+        if n % i == 0:
+            operations += i
+            n //= i
         else:
-            times += 1
-            h_count += h_count
-        n -= h_count
-    return times
+            i += 1
+    
+    operations += n
+    
+    return operations
+
