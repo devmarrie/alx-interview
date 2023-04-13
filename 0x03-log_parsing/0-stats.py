bb914fadbd40896e54812ec506a684b51fe17ac2
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-13 16:35:51 +0300

    pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Takes the logs of an input and parses it 
+"""Takes the logs of an input and parses it
    as the starndard output.
 """
 
 import sys
 
 total_size = 0
-# counts the status code per code 
-code_count = {200:0, 301:0, 400:0, 401:0, 403:0, 404:0, 405:0, 500:0}
+# counts the status code per code
+code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
     """we use try to catchthe keyboard interrupt
-       starting from the first line onwards. 
+       starting from the first line onwards.
     """
     for i, line in enumerate(sys.stdin, start=1):
         try:
@@ -39,5 +39,3 @@ except KeyboardInterrupt:
     for key, value in sorted(code_count.items()):
         if value > 0:
             print(f'{key}: {value}')
-
- 
