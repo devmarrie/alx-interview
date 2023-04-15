eb6d3db657a0ed2366afcbe16fe7f40fc905acc3
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-15 11:46:38 +0300

    final implementation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,36 +5,34 @@
 
 import sys
 
-total_size = 0
-# counts the status code per code
 code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-items = sys.stdin.readlines()
+total_size = 0
+
 
 try:
-    for i, line in enumerate(items, start=1):
-        """we use try to catch the keyboard interrupt
+    """we use try to catch the keyboard interrupt
            starting from the first line onwards.
-       """
-        try:
+    """
+    try:
+        for i, line in enumerate(sys.stdin, start=1):
             """Check the expeccted format
-            - removes urequired values
+                  - removes urequired values
             """
-            new = line.strip().split()
-            status_code = int(new[7])
-            file_size = int(new[8])
-        except ValueError:
-            continue
-        total_size += file_size
-        code_count[status_code] += 1
-        if i % 10 == 0:
-            """
-            After every 10
-            """
-            print(f'File size:{total_size}')
-            for key, value in sorted(code_count.items()):
-                if value > 0:
-                    print(f'{key}: {value}')
-            print()
+            line = line.strip().split()
+            file_size = int(line[-1])
+            total_size += file_size
+            status_code = int(line[-2])
+            code_count[status_code] += 1
+            if i % 10 == 0:
+                """
+                  After every 10
+                """
+                print(f'File size:{total_size}')
+                for key, value in sorted(code_count.items()):
+                    if value > 0:
+                        print(f'{key}: {value}')
+    except Exception as err:
+        pass
 
 except KeyboardInterrupt:
     print(f'File size:{total_size}')
