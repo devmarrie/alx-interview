82aadecee7a172c97fccafcf61d36e036d8376f6
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-15 12:28:35 +0300

    okay...

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,8 +7,9 @@ input:
     [<date>] "GET /projects/260 HTTP/1.1"
     <status code> <file size>
 """
-import sys
 
+
+import sys
 
 code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_size = 0
