a5d7fcaf2f3b9a3a82e78ee0d8b118fc8e8a2985
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-04 00:38:23 +0300

    my idea

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movie_id = process.argv[2];
+const data = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
+let people = [];
+const names = [];
+
+const checkfilms = async () => {
+    await new Promise(resolve => request(data, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+            console.log(err | res.statusCode);
+        } else {
+            const film = JSON.parse(body);
+            people = film.characters;
+            resolve();
+        }
+    }));
+};
+
+const fetchname = async () => {
+    if (people.length > 0) {
+        for (const person of people) {
+            await new Promise(resolve => request(person, (err, res, body) => {
+                if (err || res.statusCode !== 200) {
+                    console.log(err | res.statusCode);
+                } else {
+                    const character = JSON.parse(body);
+                    names.push(character.name);
+                    resolve()
+                }
+            }))
+        }
+    }
+}
+
+const display = async () => {
+    await checkfilms();
+    await fetchname();
+    if (names.length === people.length) {
+        names.forEach(name => console.log(name));
+    }
+}
+
+display()
