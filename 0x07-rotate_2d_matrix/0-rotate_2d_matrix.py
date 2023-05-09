65a385eb1fadf1ed27c203d3ccc440855dcdd71a
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-09 23:38:38 +0300

    the function

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+rotate it 90 degrees clockwise
+given a matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Takes in a 2D matrix
+    Returns nothing
+    """
+    return None
