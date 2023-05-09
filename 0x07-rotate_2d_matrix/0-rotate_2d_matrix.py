82402942cb4c6f5393c483c64be8f7a967cbb15d
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-10 00:21:32 +0300

    func implementations

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,4 +9,11 @@ def rotate_2d_matrix(matrix):
     Takes in a 2D matrix
     Returns nothing
     """
-    return None
+    n = len(matrix)
+
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+        
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
