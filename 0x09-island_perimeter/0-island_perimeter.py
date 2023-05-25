b896c4b3360203ad8d02778b22e0a81297c070d4
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-25 13:07:43 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Takes in square cells and uses them
+    to create an island
+    Returns:
+           the preimeter of the island.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    island_count = 0
+    perimeter = 0
+    if rows > 100 or cols > 100:
+        raise ValueError("Rows or columns exceeds the expected value")
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                # Minus 2 horizontally
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+                # Minus 2 vertically
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+    return perimeter
