cad2734ea5dedf063c85223b5be871c28710ec8f
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-02 02:31:23 +0300

    my approach

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,44 @@
+def isWinner(x, nums):
+    marria = 0
+    ben = 0
+    playing = []
+    prime = []
+    count = 0
+    look = {}
+    for n in nums:
+        playing.append(n)
+    
+    for y in playing:
+        for j in range(1, y + 1):
+            if y not in look:
+                look[y] = []
+            look[y].append(j)
+        #print(look)
+        
+    if count <= x:
+        for play in  playing:
+            vals = look[play]
+            count += 1
+            for num in vals:
+                if num > 1:
+                    for i in range(2, num):
+                        if (num % i) == 0:
+                            break
+                        else:
+                            prime.append(num)
+                            #print(num
+    
+    for weh in prime:
+        odd_index = prime.index(weh)
+        if (odd_index % 2) != 0:
+            marria += 1
+        else:
+            ben += 1
+    
+    if marria > ben:
+        print ('Maria')
+    else:
+        print ('Ben')
+
+
+isWinner(3, [4, 5, 1])
